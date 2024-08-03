0c1038e01b2613b6bf71462a7e130de14d0af6b4
bhargav <bhargavatrgukt@gmail.com>
2024-08-03 11:00:43 +0530

    cors added

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 // server.js
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './auth/route.js';
@@ -11,6 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser())
 
